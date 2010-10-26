d48dc979be1bc4b12099f1b9079450ea10813df1
Aslak Johannessen <aslakjo@bekk.no>
2010-10-26 23:46:44 +0200

    legger til litt test data

--- web_server/src/main/scala/no/bekk/scala/lib/TeamRegister.scala
+++ web_server/src/main/scala/no/bekk/scala/lib/TeamRegister.scala
@@ -5,6 +5,9 @@ import no.bekk.scala.messages._
 
 object TeamRegister extends TeamService
 {
-  def listTeams = List[no.bekk.scala.Team]()
-  def statusOfQuestionForTeam(team:Team):List[(Question, Option[Answer])] = List((new Question("tøyse spørsmål"), None)) 
+  def listTeams = List[no.bekk.scala.Team](new Team("test"))
+  def statusOfQuestionForTeam(team:Team):List[(Question, Option[Answer])] = List(
+    (new Question("tøyse spørsmål"), None),
+    (new Question("tøyse spørsmål"), Some(new Answer(new Team("test"), new Question("="), "Svar")))
+  ) 
 }
