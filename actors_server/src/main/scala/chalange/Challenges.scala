c152e66aa238ceda4870a8b88cd9c89c9d152658
Aslak Johannessen <aslakjo@bekk.no>
2010-11-15 23:42:28 +0100

    gir nye verdier til alle som spør

--- actors_server/src/main/scala/chalange/Challenges.scala
+++ actors_server/src/main/scala/chalange/Challenges.scala
@@ -4,20 +4,20 @@ import no.bekk.scala.messages.Question
 
 trait Challenges
 {
-  val listOfNumbers = Range(1,8).map(Random.nextInt).toList
+  def listOfNumbers() = Range(1,8).map(Random.nextInt).toList
   val symbols:List[Symbol] = List('a,'b,'c,'d,'e)
-  val listOfSymbols:List[Symbol] = Range(1,15).toList.map((i:Int)=> symbols(Random.nextInt(symbols.length)))
+  def listOfSymbols():List[Symbol] = Range(1,15).toList.map((i:Int)=> symbols(Random.nextInt(symbols.length)))
   val challenges = List(
     new Challenge("Ping", "Svar med pong",   "", (q:Question, tekst:Any) => tekst.equals("pong")),
     new Challenge("Talet etter 1 - 2- 3 - x", "Svar med tallet etter 3",  3, (q:Question, number:Any)=> number == 4),
-    new Challenge("P-01: Siste element i listen", "", listOfNumbers , (q:Question, answer:Any) => q.content.asInstanceOf[List[Int]].last.equals(answer)),
-    new Challenge("P-02: Nest siste element i listen", "" , listOfNumbers, (q:Question, answer:Any) => q.content.asInstanceOf[List[Int]].dropRight(1).last.equals(answer)),
-    new Challenge("P-03: Finn det 3 elementet i listen, det første elementet er det 0'te", "",  listOfNumbers,(q:Question, answer:Any) => q.content.asInstanceOf[List[Int]](3).equals(answer)),
-    new Challenge("P-04: Tell antall elemnter i listen", "" , listOfNumbers,(q:Question, answer:Any)=> answer.equals(q.content.asInstanceOf[List[Int]].length)),
-    new Challenge("P-05: Reverser listen", "",  listOfNumbers, (q:Question, answer:Any) => answer.equals(q.content.asInstanceOf[List[Int]].reverse)),
+    new Challenge("P-01: Siste element i listen", "", listOfNumbers() , (q:Question, answer:Any) => q.content.asInstanceOf[List[Int]].last.equals(answer)),
+    new Challenge("P-02: Nest siste element i listen", "" , listOfNumbers(), (q:Question, answer:Any) => q.content.asInstanceOf[List[Int]].dropRight(1).last.equals(answer)),
+    new Challenge("P-03: Finn det 3 elementet i listen, det første elementet er det 0'te", "",  listOfNumbers(),(q:Question, answer:Any) => q.content.asInstanceOf[List[Int]](3).equals(answer)),
+    new Challenge("P-04: Tell antall elemnter i listen", "" , listOfNumbers(),(q:Question, answer:Any)=> answer.equals(q.content.asInstanceOf[List[Int]].length)),
+    new Challenge("P-05: Reverser listen", "",  listOfNumbers(), (q:Question, answer:Any) => answer.equals(q.content.asInstanceOf[List[Int]].reverse)),
     new Challenge("P-07: Flat ut listene til en liste", "",  List(List(1, 1), List(2), List(3)), (q:Question, answer:Any) => answer.equals(List(1, 1, 2, 3))),
-    new Challenge("P-08: Fjern like elementer, hvis de kommer etterhveranre", "" , listOfSymbols, (q:Question, answer:Any) => answer.equals(fjernLikeRepiterendeElementer(listOfSymbols))),
-    new Challenge("P-09: Legg like elementer som kommer etter hverandre i hver sin liste", "",  listOfSymbols, (q:Question, answer:Any) => answer.equals(leggLikeIListe(listOfSymbols)))
+    new Challenge("P-08: Fjern like elementer, hvis de kommer etterhveranre", "" , listOfSymbols(), (q:Question, answer:Any) => answer.equals(fjernLikeRepiterendeElementer(q.content.asInstanceOf[List[Symbol]]))),
+    new Challenge("P-09: Legg like elementer som kommer etter hverandre i hver sin liste", "",  listOfSymbols(), (q:Question, answer:Any) => answer.equals(leggLikeIListe(q.content.asInstanceOf[List[Symbol]])))
   )
 
   def fjernLikeRepiterendeElementer(list:List[Symbol])={
