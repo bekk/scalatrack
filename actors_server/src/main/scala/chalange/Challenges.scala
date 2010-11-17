f5f90e1313294fe2fa79b7e3f7f1d8bf4a25c525
Aslak Johannessen <aslakjo@bekk.no>
2010-11-17 18:13:34 +0100

    bruker ekstraktors i stedet for casts

--- actors_server/src/main/scala/chalange/Challenges.scala
+++ actors_server/src/main/scala/chalange/Challenges.scala
@@ -1,24 +1,48 @@
 package no.bekk.scala
 import scala.util.Random
-import no.bekk.scala.messages.Question
+import no.bekk.scala.messages._
+
 
 trait Challenges
 {
-  def listOfNumbers() = Range(1,8).map(Random.nextInt).toList
+  def listOfNumbers(amount: Int = 8) = Range(1,amount).map(Random.nextInt).toList
   val symbols:List[Symbol] = List('a,'b,'c,'d,'e)
   def listOfSymbols():List[Symbol] = Range(1,15).toList.map((i:Int)=> symbols(Random.nextInt(symbols.length)))
   def challenges() = List(
     new Challenge("Ping", "Svar med pong",   "", (q:Question, tekst:Any) => tekst.equals("pong")),
     new Challenge("Talet etter 1 - 2- 3 - x", "Svar med tallet etter 3",  3, (q:Question, number:Any)=> number == 4),
-    new Challenge("P-01: Siste element i listen", "", listOfNumbers() , (q:Question, answer:Any) => q.content.asInstanceOf[List[Int]].last.equals(answer)),
-    new Challenge("P-02: Nest siste element i listen", "" , listOfNumbers(), (q:Question, answer:Any) => q.content.asInstanceOf[List[Int]].dropRight(1).last.equals(answer)),
-    new Challenge("P-03: Finn det 3 elementet i listen, det første elementet er det 0'te", "",  listOfNumbers(),(q:Question, answer:Any) => q.content.asInstanceOf[List[Int]](3).equals(answer)),
-    new Challenge("P-04: Tell antall elemnter i listen", "" , listOfNumbers(),(q:Question, answer:Any)=> answer.equals(q.content.asInstanceOf[List[Int]].length)),
-    new Challenge("P-05: Reverser listen", "",  listOfNumbers(), (q:Question, answer:Any) => answer.equals(q.content.asInstanceOf[List[Int]].reverse)),
+    new Challenge("P-01: Siste element i listen", "", listOfNumbers() , (q:Question, answer:Any) => q.content match{
+          case IntList(l) => answer.equals(l.last)
+          case _ => false
+    }),
+    new Challenge("P-02: Nest siste element i listen", "" , listOfNumbers(), (q:Question, answer:Any) => q.content match{
+            case IntList(l) => l.dropRight(1).last.equals(answer)
+            case _ => false
+    }),
+    new Challenge("P-03: Finn det 3 elementet i listen, det første elementet er det 0'te", "",  listOfNumbers(),(q:Question, answer:Any) => q.content match {
+            case IntList(l) => l(3).equals(answer)
+            case _ => false
+    }),
+    new Challenge("P-04: Tell antall elemnter i listen", "" , listOfNumbers(Random.nextInt(20)),(q:Question, answer:Any)=> q.content match{
+            case IntList(l) => answer.equals(l.length)
+            case _ => false
+    }),
+    new Challenge("P-05: Reverser listen", "",  listOfNumbers(), (q:Question, answer:Any) => q.content match {
+            case IntList(l) => answer.equals(l.reverse)
+            case _ => false
+    }),
     new Challenge("P-07: Flat ut listene til en liste", "",  List(List(1, 1), List(2), List(3)), (q:Question, answer:Any) => answer.equals(List(1, 1, 2, 3))),
-    new Challenge("P-08: Fjern like elementer, hvis de kommer etterhveranre", "" , listOfSymbols(), (q:Question, answer:Any) => answer.equals(fjernLikeRepiterendeElementer(q.content.asInstanceOf[List[Symbol]]))),
-    new Challenge("P-09: Legg like elementer som kommer etter hverandre i hver sin liste", "",  listOfSymbols(), (q:Question, answer:Any) => answer.equals(leggLikeIListe(q.content.asInstanceOf[List[Symbol]])))
+    new Challenge("P-08: Fjern like elementer, hvis de kommer etterhveranre", "" , listOfSymbols(), (q:Question, answer:Any) => q.content match{
+            case SymbolList(l) => answer.equals(fjernLikeRepiterendeElementer(l))
+            case _ =>false
+    }),
+    new Challenge("P-09: Legg like elementer som kommer etter hverandre i hver sin liste", "",  listOfSymbols(), (q:Question, answer:Any) => q.content match{
+            case SymbolList(l) => answer.equals(leggLikeIListe(l))
+            case _ => false
+    })
   )
+
+ 
 
   def fjernLikeRepiterendeElementer(list:List[Symbol])={
     list.foldRight(List[Symbol]())((next ,collected)=> {
